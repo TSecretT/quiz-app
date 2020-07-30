fe96111ef0ecf6dbf007b42eb781ea0e8e072296
TSecretT <timichfull@gmail.com>
2020-07-30 18:20:14 +0600

    homework 1+2+3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+var questions = [
+	{
+		text: "5+5",
+		answer : "10",
+		multipleType: false
+	},
+	{
+		text:"100+50",
+		answer: "150",
+		multipleType: false
+	},
+	{
+		text: "6*4",
+		answer: "24",
+		multipleType: false
+	},
+	{
+		text: "18/9",
+		answer : "2",
+		multipleType: false
+	},
+	{
+		text: "Capital of Kazakhstan:",
+		choices: ["Nur-Sultan", "Almaty", "Shymkent"],
+		answer: "Nur-Sultan",
+		multipleType: true
+	},
+	{
+		text: "This page is build on:",
+		choices: ["Swift", "React", "Lego"],
+		answer: "React",
+		multipleType: true
+	},
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [text, setText] = React.useState("");
+	const [question, setQuestion] = React.useState({});
+	
+	function getRandomQuestion(){
+		setQuestion(questions[Math.floor(Math.random() * questions.length)])
+	}
+
+	React.useEffect(() => {
+		getRandomQuestion();
+	}, [])
+	
+
+	return (
+		<div>
+			<div>{question.text}</div>
+			{question.multipleType? (
+				<>
+				<p>Choices:</p>
+				{question.choices.map((choice, i) => {return <p>{i+1}) {choice}</p>})}
+				</>
+			): null}
+			<input
+				value={text}
+				onChange={(event) => {
+				setText(event.target.value);
+				}}
+			>
+			</input>
+			<button
+				onClick={() => {setText("")}}
+			>
+				Reset
+			</button>
+			<button
+				onClick={() => {
+				if(text===question.answer){
+					alert("Correct!")
+				} else {
+					alert("Inorrect!")
+				}
+				getRandomQuestion();
+				setText("")
+				}}
+			>
+				Submit
+			</button>
+		</div>
+	);
 }
 
 export default App;
